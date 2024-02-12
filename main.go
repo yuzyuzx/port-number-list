388f67fd106650c331c2a45afb91439a392a3886
yuz <yuzyuzx@gmail.com>
2024-02-12 22:22:32 +0900

    リファクタ

--- main.go
+++ main.go
@@ -1,24 +1,62 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "strings"
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
 )
 
-const portNumberText = "./port-number.txt"
+const (
+	// データ保存ディレクトリ
+	dataDir = "data"
+	// データファイル
+	portNumberText = "port-number.txt"
+)
 
 func main() {
-  data, _ := os.Open(portNumberText)
-  defer data.Close()
+	dataFile, err := createFilePath()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-  scanner := bufio.NewScanner(data)
+	// ファイルを用意
+	data, _ := os.Open(dataFile)
+	defer data.Close()
 
-  for scanner.Scan() {
-    s := scanner.Text()
-    s = strings.ReplaceAll(s, "\t", ",")
-    fmt.Println(s)
-  }
+	// スキャナを用意
+	// トークンは行
+	scanner := bufio.NewScanner(data)
 
+	// EOF（ファイル末尾）になるまでスキャンを繰り返す
+	for scanner.Scan() {
+		// スキャンした内容を文字列で取得
+		// 返り値に行末尾の改行文字は含まれない
+		s := scanner.Text()
+
+		// タブをカンマに置換
+		s = strings.ReplaceAll(s, "\t", ",")
+
+		fmt.Println(s)
+	}
+
+	// ファイル終端に正常に到達したか
+	// 正常にファイル末尾まで読み込まれたらerrはnilを返す
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "scanner.Err()")
+	}
+
+}
+
+// ファイルパスを文字列で組み立てる
+func createFilePath() (string, error) {
+	// カレントディレクトリを取得
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return "", errors.New("createFilePath()")
+	}
+
+	return fmt.Sprintf("%s/%s/%s", currentDir, dataDir, portNumberText), nil
 }
