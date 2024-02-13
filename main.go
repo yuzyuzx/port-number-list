0ed61c5883b878bc788750e6642e36f40499d974
yuz <yuzyuzx@gmail.com>
2024-02-13 15:40:37 +0900

    ファイル作成から保存処理までを実装

--- main.go
+++ main.go
@@ -23,12 +23,19 @@ func main() {
 	}
 
 	// ファイルを用意
-	data, _ := os.Open(dataFile)
+	data, err := os.Open(dataFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	defer data.Close()
 
 	// スキャナを用意
 	// トークンは行
 	scanner := bufio.NewScanner(data)
+
+  // ファイルの各行を格納する配列
+  rowText := []string{}
 
 	// EOF（ファイル末尾）になるまでスキャンを繰り返す
 	for scanner.Scan() {
@@ -39,14 +46,35 @@ func main() {
 		// タブをカンマに置換
 		s = strings.ReplaceAll(s, "\t", ",")
 
-		fmt.Println(s)
+    // 文字列を格納する
+    rowText = append(rowText, s)
+
 	}
 
 	// ファイル終端に正常に到達したか
 	// 正常にファイル末尾まで読み込まれたらerrはnilを返す
 	if err := scanner.Err(); err != nil {
 		fmt.Println("scanner.Err()")
+    return
 	}
+
+  // 配列に格納している文字列を改行区切りでつなげる
+  joinedText := strings.Join(rowText, "\n")
+  fmt.Println(joinedText)
+
+  // 保存用ファイルを新規作成
+  newFile, err := os.Create("data/result.csv")
+  if err != nil {
+    fmt.Println("新規ファイル作成に失敗しました")
+    return
+  }
+
+  // 作成したファイルに読み込んだ文字列を書き込む
+  if _, err := newFile.WriteString(joinedText); err != nil {
+    fmt.Println("ファイルへの書き込みに失敗しました")
+  }
+
+
 }
 
 // ファイルパスを文字列で組み立てる
